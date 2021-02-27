c75e7dde475e65169b891f19e40fc82c0266a7f2
Yaritza Marlene Garcia Gomez <ygomez9@gatech.edu>
2021-02-26 21:30:35 -0500

    updated configurationScreen and added temp handler

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,8 +1,12 @@
 package controller;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 import model.GameModel;
 import view.StartScreen;
@@ -11,6 +15,8 @@ import view.ConfigurationScreen;
 public class Controller extends Application {
     private Stage mainWindow;
     private GameModel gameModel;
+    private int difficultyLevel;
+    private String weaponChoice;
     private final int width = 600;
     private final int height = 600;
 
@@ -34,18 +40,51 @@ public class Controller extends Application {
 
     private void goToConfigurationScreen() {
         ConfigurationScreen screen = new ConfigurationScreen(width, height);
-        /// *** Needs to change
-        Button decrButton = screen.getDecrButton();
-        decrButton.setOnAction(e -> {
-            gameModel.decrementState();
-            screen.updateLabel(gameModel.getState());
+
+        Button easyButton = screen.getEasyButton();
+        Button mediumButton = screen.getMediumButton();
+        Button hardButton = screen.getHardButton();
+        Button wipeCrossbowButton = screen.getWipeCrossbowButton();
+        Button disinfectantSprayButton = screen.getDisinfectantSprayButton();
+        Button thermometerSwordButton = screen.getThermometerSwordButton();
+        TextField nameField = screen.getNameField();
+        easyButton.setOnAction( e -> difficultyLevel = 1);
+        mediumButton.setOnAction( e -> difficultyLevel = 2);
+        hardButton.setOnAction( e -> difficultyLevel = 3);
+
+        wipeCrossbowButton.setOnAction( e -> {
+//            System.out.println(nameField.getText());
+//            System.out.println("weapon: wipe crossbow ");
+            weaponChoice = "wipe crossbow";
         });
-        Button incrButton = screen.getIncrButton();
-        incrButton.setOnAction(e -> {
-            gameModel.incrementState();
-            screen.updateLabel(gameModel.getState());
+        disinfectantSprayButton.setOnAction( e -> {
+//            System.out.println(nameField.getText());
+//            System.out.println("weapon: disinfectant spray ");
+            weaponChoice = "disinfectant spray";
         });
-        /// *** stop changes
+        thermometerSwordButton.setOnAction( e -> {
+//            System.out.println(nameField.getText());
+//            System.out.println("weapon: thermometer sword ");
+            weaponChoice = "thermometer sword";
+        });
+
+//        nameField.setOnKeyPressed(new EventHandler<KeyEvent>() {
+//            @Override
+//            public void handle(KeyEvent event) {
+//                boolean validName = false;
+//                while (validName == false) {
+//                    if (event.getCode() == KeyCode.ENTER) {
+//                        String name = nameField.getText();
+//                        if (name == null || name.trim().isEmpty()) {
+//                            nameField.setPromptText("Invalid Name");
+//                        } else {
+//                            validName = true;
+//                        }
+//                    }
+//                }
+//                System.out.print("launch initial game screen");
+//            }
+//        });
 
         Scene scene = screen.getScene();
         mainWindow.setScene(scene);
--- src/view/ConfigurationScreen.java
+++ src/view/ConfigurationScreen.java
@@ -1,45 +1,159 @@
 package view;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 public class ConfigurationScreen {
     private int width;
     private int height;
-    private Button incrButton;
-    private Button decrButton;
-    private Label label;
+    private Button easyButton;
+    private Button mediumButton;
+    private Button hardButton;
+    private Button wipeCrossbowButton;
+    private Button disinfectantSprayButton;
+    private Button thermometerSwordButton;
+    private TextField nameField;
+    private Label enterYourName;
+    private Label chooseDifficultyLevel;
+    private Label selectYourWeapon;
+
 
     private ConfigurationScreen() {}
     public ConfigurationScreen(int width, int height) {
         this.width = width;
         this.height = height;
-        incrButton = new Button("+");
-        decrButton = new Button("-");
-        label = new Label("We are in the second screen now!");
+        easyButton = new Button("easy");
+        mediumButton = new Button("medium");
+        hardButton = new Button("hard");
+        wipeCrossbowButton = new Button();
+        disinfectantSprayButton = new Button();
+        thermometerSwordButton = new Button();
+        nameField = new TextField();
+        enterYourName = new Label("Enter Your Name");
+        chooseDifficultyLevel = new Label("Choose Difficulty Level");
+        selectYourWeapon = new Label("Select Your Weapon");
     }
     public Scene getScene() {
-        label.getStyleClass().add("statusText");
-        HBox buttons = new HBox(decrButton, incrButton);
-        buttons.getStyleClass().add("buttons");
-        VBox vbox = new VBox(label, buttons);
-        Scene scene = new Scene(vbox, width, height);
-        scene.getStylesheets().add("file:resources/css/SecondScreen.css");
-        return scene;
+        Font dogicaFont = Font.loadFont("file:resources/dogica/TTF/dogicapixel.ttf", 20);
+        Font dogicaFontBold = Font.loadFont( "file:resources/dogica/TTF/dogicapixelbold.ttf", 23);
+        //***** Enter Your Name ******
+        //setting up "Enter Your Name" Label
+        enterYourName.setFont(dogicaFontBold);
+        //setting up graphic for name field
+        ImageView nameTextFeildImgView = new ImageView(new Image("file:resources/pngs/nameTextBox.png"));
+        nameTextFeildImgView.setFitWidth(400);
+        nameTextFeildImgView.setPreserveRatio(true);
+        //setting up the name field
+        nameField.setFont(dogicaFont);
+        nameField.setMaxWidth(380);
+        nameField.setPrefHeight(50);
+        nameField.setStyle("-fx-background-radius: 10 10 10 10");
+        nameField.setStyle("-fx-background-color: #ebd6b4");
+        //Stack pane to impose name field over the graphic
+        StackPane nameFeildPane = new StackPane();
+        nameFeildPane.getChildren().addAll(nameTextFeildImgView, nameField);
+        //Vertical box to hold label and name feild
+        VBox namePane = new VBox(15);
+        namePane.setAlignment(Pos.CENTER);
+
+        namePane.getChildren().addAll(enterYourName, nameFeildPane);
+
+        //***** Choose Difficulty Level *****
+        //setting up level difficulty label
+        chooseDifficultyLevel.setFont(dogicaFontBold);
+        chooseDifficultyLevel.setAlignment(Pos.CENTER);
+        chooseDifficultyLevel.setPrefWidth(500);
+        //setting up easy difficulty button
+        easyButton.setFont(dogicaFont);
+        easyButton.setStyle("-fx-background-color: #ebd6b4");
+        //setting up medium difficulty button
+        mediumButton.setFont(dogicaFont);
+        mediumButton.setStyle("-fx-background-color: #ebd6b4");
+        //setting up hard difficulty label
+        hardButton.setFont(dogicaFont);
+        hardButton.setStyle("-fx-background-color: #ebd6b4");
+        //Horizontal box to hold the level difficulty buttons
+        HBox levelButtonBox = new HBox(75);
+        levelButtonBox.getChildren().addAll(easyButton, mediumButton, hardButton);
+        levelButtonBox.setAlignment(Pos.CENTER);
+        //Vertical box to hold the level difficulty label and buttons
+        VBox levelBox = new VBox(25);
+//        levelBox.setPadding(new Insets(10,0,10,0));
+        levelBox.setAlignment(Pos.CENTER);
+        levelBox.getChildren().addAll(chooseDifficultyLevel, levelButtonBox);
+
+        //***** Select Your Weapon *****
+        //setting up weapon title
+        selectYourWeapon.setFont(dogicaFontBold);
+        //setting up wipeCrossbow weapon button
+        ImageView wipeCrossbow = new ImageView(new Image("file:resources/pngs/Grocery Quest Screens-08.png"));
+        wipeCrossbow.setFitWidth(140);
+        wipeCrossbow.setPreserveRatio(true);
+        wipeCrossbowButton.setGraphic(wipeCrossbow);
+        wipeCrossbowButton.setStyle("-fx-background-color: #ebd6b4");
+        //setting up disinfectantSpray weapon button
+        ImageView disinfectantSpray = new ImageView(new Image("file:resources/pngs/Grocery Quest Screens-10.png"));
+        disinfectantSpray.setFitWidth(140);
+        disinfectantSpray.setPreserveRatio(true);
+        disinfectantSprayButton.setGraphic(disinfectantSpray);
+        disinfectantSprayButton.setStyle("-fx-background-color: #ebd6b4");
+        //setting up thermometerSword weapon button
+        ImageView thermometerSword = new ImageView(new Image("file:resources/pngs/Grocery Quest Screens-11.png"));
+        thermometerSword.setFitWidth(140);
+        thermometerSword.setPreserveRatio(true);
+        thermometerSwordButton.setGraphic(thermometerSword);
+        thermometerSwordButton.setStyle("-fx-background-color: #ebd6b4");
+        //Horizontal box pane to hold the weapon buttons
+        HBox weaponListBox = new HBox(15);
+        weaponListBox.setAlignment(Pos.CENTER);
+        weaponListBox.getChildren().addAll(wipeCrossbowButton, disinfectantSprayButton, thermometerSwordButton);
+        weaponListBox.setStyle("-fx-background-color: #ebd6b4");
+        //Vertical box pane to hold the weapon title and buttons
+        VBox weaponBox = new VBox(18);
+        weaponBox.setAlignment(Pos.CENTER);
+        weaponBox.getChildren().addAll(selectYourWeapon, weaponListBox);
+
+        //base holds all the other panes and goes into the cofig scene
+        VBox base = new VBox(25);
+        base.setPadding(new Insets(50,0,50,0));
+
+        base.getChildren().addAll(namePane,levelBox,weaponBox);
+        base.setStyle("-fx-background-color: #ebd6b4");
+
+        Scene configurationScene = new Scene(base, width,height);
+
+        return configurationScene;
     }
 
-    public Button getIncrButton() {
-        return incrButton;
+    public Button getEasyButton() {
+        return easyButton;
     }
 
-    public Button getDecrButton() {
-        return decrButton;
+    public Button getMediumButton() {
+        return mediumButton;
     }
 
-    public void updateLabel(int state) {
-        label.setText(Integer.toString(state));
-    }
+    public Button getHardButton() { return hardButton; }
+
+    public Button getWipeCrossbowButton() { return wipeCrossbowButton; }
+
+    public Button getDisinfectantSprayButton() { return disinfectantSprayButton; }
+
+    public Button getThermometerSwordButton() { return thermometerSwordButton; }
+
+    public TextField getNameField() { return nameField;}
+
+//    public void updateLabel(int state) {
+//        label.setText(Integer.toString(state));
+//    }
 }
