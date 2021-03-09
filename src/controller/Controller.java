10815b6d2295b4c4f126d8d54163a53d702c7cc7
Chu, Claudia Diane <claudiachu@gatech.edu>
2021-03-09 10:08:46 -0500

    Update Controller.java

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -209,6 +209,26 @@ public class Controller extends Application {
         mainWindow.show();
     }
 
+        /**
+     * This method launches and provides event handling for
+     * the Win Screen
+     */
+    private void initWinScreen() {
+        gameModel.setState("Win Screen");
+        EndScreen screen = new EndScreen(width, height);
+        Button replayButton = screen.getReplayButton();
+        replayButton.setOnAction(e -> {
+            initStartScreen();
+        });
+        Button exitButton = screen.getExitButton();
+        exitButton.setOnAction(e -> {
+            System.exit(0);
+        });
+        Scene scene = screen.getScene();
+        mainWindow.setScene(scene);
+        mainWindow.show();
+    }
+    
     public String getState() {
         return gameModel.getState();
     }
