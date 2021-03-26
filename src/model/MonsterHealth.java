57180b06c2f62843b7092ca45069a7a2a59423c5
Chinar Dankhara <cdankhara3@gatech.edu>
2021-03-26 15:55:06 -0400

    adding basic monster class and health functionality

---
+++ src/model/MonsterHealth.java
@@ -0,0 +1,44 @@
+package model;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class MonsterHealth {
+    private int healthLevel; //current health level
+    private int healthHitChange;
+    private final double barWidth;
+    private static final ImageView[] HEALTHBARS = {
+            new ImageView(new Image("file:resources/pngs/EnemyHealthBar0.png")),
+            new ImageView(new Image("file:resources/pngs/EnemyHealthBar1.png")),
+            new ImageView(new Image("file:resources/pngs/EnemyHealthBar2.png")),
+            new ImageView(new Image("file:resources/pngs/EnemyHealthBar3.png")),
+            new ImageView(new Image("file:resources/pngs/EnemyHealthBar4.png"))
+    };
+
+    public MonsterHealth(int healthLevel, int healthHitChange, double width) {
+        this.healthLevel = healthLevel;
+        this.healthHitChange = healthHitChange;
+        this.barWidth = width;
+    }
+
+    public ImageView hit(int weaponHit) {
+        int change = weaponHit * healthHitChange;
+        if (healthLevel < change) {
+            this.healthLevel = 0;
+        } else {
+            this.healthLevel -= change;
+        }
+        return barSettings(HEALTHBARS[healthLevel]);
+    }
+
+    public ImageView getCurrentHealthBar() {
+        return barSettings(HEALTHBARS[healthLevel]);
+    }
+
+    private ImageView barSettings(ImageView bar) {
+        bar.setFitWidth(barWidth);
+        bar.setPreserveRatio(true);
+        bar.setId("health");
+        return bar;
+    }
+}
---
+++ src/model/MonsterType.java
@@ -0,0 +1,9 @@
+package model;
+
+public enum MonsterType {
+    FA_ANTIMASKER,
+    MA_ANTIMASKER,
+    COVID_BOSS_LARGE,
+    COVID_BOSS_SMALL
+}
+
