06e1c959df5671127269c3aca810065563e40904
jsk56143 <jsk56143@uga.edu>
2021-03-28 13:04:48 -0400

    Need to complete Javadoc

--- src/model/Exit.java
+++ src/model/Exit.java
@@ -1,5 +1,10 @@
 package model;
 
+/**
+ * The Exit Class
+ *
+ * The Exit Class helps the player move between levels.
+ */
 public class Exit {
     private ExitType side1;
     private ExitType side2;
@@ -7,6 +12,16 @@ public class Exit {
     private String screen2;
     private Boolean isOpen;
 
+    //TODO: I'm not too familiar with this class. Please add Javadoc. -Josh
+    /**
+     * Exit Constructor
+     *
+     * @param side1
+     * @param side2
+     * @param screen1
+     * @param screen2
+     * @param isOpen
+     */
     public Exit(ExitType side1, ExitType side2, String screen1, String screen2, Boolean isOpen) {
         this.side1 = side1;
         this.side2 = side2;
@@ -15,6 +30,11 @@ public class Exit {
         this.isOpen = isOpen;
     }
 
+    /**
+     *
+     * @param iD
+     * @return
+     */
     public ExitType getExitType(String iD) {
         if (iD.equals(screen1)) {
             return side1;
@@ -23,6 +43,12 @@ public class Exit {
         }
     }
 
+    /**
+     *
+     *
+     * @param model
+     * @return
+     */
     public String getAdjacentState(GameModel model) {
         if (model.getState().equals(screen1)) {
             return screen2;
@@ -31,10 +57,18 @@ public class Exit {
         }
     }
 
+    /**
+     *
+     * @return true if open, false otherwise
+     */
     public Boolean getIsOpen() {
         return isOpen;
     }
 
+    /**
+     *
+     * @param isOpen isOpen condition to set
+     */
     public void setIsOpen(Boolean isOpen) {
         this.isOpen = isOpen;
     }
