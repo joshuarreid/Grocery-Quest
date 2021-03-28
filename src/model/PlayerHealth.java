e0d0a0aaeb210187e104f50356028ca5c18ed378
jsk56143 <jsk56143@uga.edu>
2021-03-28 11:42:49 -0400

    Added javadoc to PlayerHealth

--- src/model/PlayerHealth.java
+++ src/model/PlayerHealth.java
@@ -1,11 +1,21 @@
 package model;
 
-
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
+/**
+ * The PlayerHealth Class
+ *
+ * The PlayerHealth Class is responsible for keeping track of the
+ * health of the player internally and visually.
+ *
+ * healthLevel - the player's current health level
+ * healthHitChange - how much the health should decrease depending on the difficulty of the level
+ * barWidth - the width of the healthbar
+ * HEALTHBARS - all the imageviews of the healthbar from empty to full
+ */
 public class PlayerHealth {
-    private int healthLevel; //current health level
+    private int healthLevel;
     private int healthHitChange;
     private final double barWidth;
     private static final ImageView[] HEALTHBARS = {
@@ -21,12 +31,25 @@ public class PlayerHealth {
         new ImageView(new Image("file:resources/pngs/HealthBar9.png")),
         new ImageView(new Image("file:resources/pngs/HealthBar10.png"))};
 
+    /**
+     * PlayerHealth Constructor
+     *
+     * @param difficultyLevel the difficulty level
+     * @param width the width of the bar
+     */
     public PlayerHealth(int difficultyLevel, double width) {
         this.healthLevel = 10;
         this.healthHitChange = difficultyLevel;
         this.barWidth = width;
     }
 
+    /**
+     * Decreases the health of the player internally and
+     * visually
+     *
+     * @param power damage that the player should incur
+     * @return the updated "dcreased" imageview
+     */
     public ImageView removeHealth(int power) {
         int change = power * healthHitChange;
         if (healthLevel < change) {
@@ -37,6 +60,13 @@ public class PlayerHealth {
         return barSettings(HEALTHBARS[healthLevel]);
     }
 
+    /**
+     * Increases the health of the player internally and
+     * visually
+     *
+     * @param amount the amount to increase by
+     * @return the updated "increased" imageview
+     */
     public ImageView addHealth(int amount) {
         int newHealth = this.healthLevel + amount;
         if (newHealth > 10) {
@@ -45,12 +75,26 @@ public class PlayerHealth {
         return barSettings(HEALTHBARS[healthLevel]);
     }
 
+    /**
+     *
+     * @return the current health bar imageview
+     */
     public ImageView getCurrentHealthBar() {
         return barSettings(HEALTHBARS[healthLevel]);
     }
 
+    /**
+     *
+     * @return the health level
+     */
     public int getPlayerHealthLevel() { return healthLevel; }
 
+    /**
+     * Sets some attributes to the health bar
+     *
+     * @param bar the bar to modify
+     * @return bar
+     */
     private ImageView barSettings(ImageView bar) {
         bar.setFitWidth(barWidth);
         bar.setPreserveRatio(true);
