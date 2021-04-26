7adb2c349706bc35b23a14329aa74aced5695716
Yaritza Marlene Garcia Gomez <ygomez9@gatech.edu>
2021-04-25 23:11:45 -0400

    claudia and marlene compromise

--- src/view/ChallengeScreenSecond.java
+++ src/view/ChallengeScreenSecond.java
@@ -5,58 +5,63 @@ import javafx.scene.image.ImageView;
 import model.*;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ChallengeScreenSecond extends ChallengeScreen{
+    private boolean loadObj;
     public ChallengeScreenSecond(Player hero, LevelRandomizer lr, Exit[] exits,
                                  ArrayList<Monster> monsters, ArrayList<Collectable> items) {
         super(hero, lr, exits,"Challenge 2", monsters, items);
+        Random rd = new Random();
+        loadObj = rd.nextBoolean();
     }
 
     @Override
     void loadObjects() {
         //claudia add tings
+        if(loadObj) {
+            for (int i = 6; i < 16; i += 2) {
+                ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle2.png"));
+                aisle.setFitWidth(60);
+                aisle.setFitHeight(30);
+                super.board.addObject(aisle, "AisleTop" + i, true, 0, 1, i, 2);
+            }
 
-        for(int i = 6; i < 16; i+=2) {
-            ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle2.png"));
-            aisle.setFitWidth(60);
-            aisle.setFitHeight(30);
-            super.board.addObject(aisle, "AisleTop"+i, true, 0, 1, i, 2);
+            for (int i = 0; i < 16; i += 2) {
+                ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle2.png"));
+                aisle.setFitWidth(60);
+                aisle.setFitHeight(30);
+                super.board.addObject(aisle, "AisleBottom" + i, true, 18, 1, i, 2);
+            }
+
+            for (int i = 0; i < 19; i += 18) {
+                ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle3.png"));
+                aisle.setFitWidth(90);
+                aisle.setFitHeight(30);
+                super.board.addObject(aisle, "AisleBottom3" + i, true, i, 1, 16, 3);
+            }
+
+            ImageView aisle53 = new ImageView(new Image("file:resources/pngs/Aisle5.png"));
+            aisle53.setFitWidth(30);
+            aisle53.setFitHeight(150);
+            super.board.addObject(aisle53, "Aisle5.3", true, 1, 5, 18, 1);
+
+            for (int i = 6; i < 18; i += 4) {
+                ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle4.png"));
+                aisle.setFitWidth(30);
+                aisle.setFitHeight(120);
+                super.board.addObject(aisle, "Aisle4" + i, true, i, 4, 18, 1);
+            }
+
+            ImageView aisle5 = new ImageView(new Image("file:resources/pngs/Aisle5.png"));
+            aisle5.setFitWidth(30);
+            aisle5.setFitHeight(150);
+            super.board.addObject(aisle5, "Aisle5.1", true, 2, 5, 0, 1);
+
+            ImageView aisle52 = new ImageView(new Image("file:resources/pngs/Aisle5.png"));
+            aisle52.setFitWidth(30);
+            aisle52.setFitHeight(150);
+            super.board.addObject(aisle52, "Aisle5.2", true, 13, 5, 0, 1);
         }
-
-        for(int i = 0; i < 16; i+=2) {
-            ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle2.png"));
-            aisle.setFitWidth(60);
-            aisle.setFitHeight(30);
-            super.board.addObject(aisle, "AisleBottom"+i, true, 18, 1, i, 2);
-        }
-
-        for(int i = 0; i < 19; i += 18){
-            ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle3.png"));
-            aisle.setFitWidth(90);
-            aisle.setFitHeight(30);
-            super.board.addObject(aisle, "AisleBottom3"+i, true, i, 1, 16, 3);
-        }
-
-        ImageView aisle53 = new ImageView(new Image("file:resources/pngs/Aisle5.png"));
-        aisle53.setFitWidth(30);
-        aisle53.setFitHeight(150);
-        super.board.addObject(aisle53, "Aisle5.3", true, 1, 5, 18, 1);
-
-        for(int i = 6; i < 18; i+=4) {
-            ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle4.png"));
-            aisle.setFitWidth(30);
-            aisle.setFitHeight(120);
-            super.board.addObject(aisle, "Aisle4" + i, true, i, 4, 18, 1);
-        }
-
-        ImageView aisle5 = new ImageView(new Image("file:resources/pngs/Aisle5.png"));
-        aisle5.setFitWidth(30);
-        aisle5.setFitHeight(150);
-        super.board.addObject(aisle5, "Aisle5.1", true, 2, 5, 0, 1);
-
-        ImageView aisle52 = new ImageView(new Image("file:resources/pngs/Aisle5.png"));
-        aisle52.setFitWidth(30);
-        aisle52.setFitHeight(150);
-        super.board.addObject(aisle52, "Aisle5.2", true, 13, 5, 0, 1);
     }
 }
--- src/view/ChallengeScreenThird.java
+++ src/view/ChallengeScreenThird.java
@@ -5,52 +5,58 @@ import javafx.scene.image.ImageView;
 import model.*;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class ChallengeScreenThird extends ChallengeScreen{
+    private boolean loadObj;
     public ChallengeScreenThird(Player hero, LevelRandomizer lr, Exit[] exits,
                                 ArrayList<Monster> monsters, ArrayList<Collectable> items) {
         super(hero, lr, exits,"Challenge 3", monsters, items);
+        Random rd = new Random();
+        loadObj = rd.nextBoolean();
     }
 
     @Override
     void loadObjects() {
         //claudia add tings
-        for(int i = 6; i < 16; i+=2) {
-            ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle2.png"));
-            aisle.setFitWidth(60);
-            aisle.setFitHeight(30);
-            super.board.addObject(aisle, "AisleTop"+i, true, 0, 1, i, 2);
+        if(loadObj) {
+            for (int i = 6; i < 16; i += 2) {
+                ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle2.png"));
+                aisle.setFitWidth(60);
+                aisle.setFitHeight(30);
+                super.board.addObject(aisle, "AisleTop" + i, true, 0, 1, i, 2);
+            }
+
+            for (int i = 0; i < 16; i += 2) {
+                ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle2.png"));
+                aisle.setFitWidth(60);
+                aisle.setFitHeight(30);
+                super.board.addObject(aisle, "AisleBottom" + i, true, 18, 1, i, 2);
+            }
+
+            for (int i = 0; i < 19; i += 18) {
+                ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle3.png"));
+                aisle.setFitWidth(90);
+                aisle.setFitHeight(30);
+                super.board.addObject(aisle, "AisleBottom3" + i, true, i, 1, 16, 3);
+            }
+
+            for (int i = 2; i < 18; i += 4) {
+                ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle4.png"));
+                aisle.setFitWidth(30);
+                aisle.setFitHeight(120);
+                super.board.addObject(aisle, "Aisle4" + i, true, i, 4, 0, 1);
+            }
+
+            ImageView aisle5 = new ImageView(new Image("file:resources/pngs/Aisle5.png"));
+            aisle5.setFitWidth(30);
+            aisle5.setFitHeight(150);
+            super.board.addObject(aisle5, "Aisle5.1", true, 1, 5, 18, 1);
+
+            ImageView aisle52 = new ImageView(new Image("file:resources/pngs/Aisle5.png"));
+            aisle52.setFitWidth(30);
+            aisle52.setFitHeight(150);
+            super.board.addObject(aisle52, "Aisle5.2", true, 13, 5, 18, 1);
         }
-
-        for(int i = 0; i < 16; i+=2) {
-            ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle2.png"));
-            aisle.setFitWidth(60);
-            aisle.setFitHeight(30);
-            super.board.addObject(aisle, "AisleBottom"+i, true, 18, 1, i, 2);
-        }
-
-        for(int i = 0; i < 19; i += 18){
-            ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle3.png"));
-            aisle.setFitWidth(90);
-            aisle.setFitHeight(30);
-            super.board.addObject(aisle, "AisleBottom3"+i, true, i, 1, 16, 3);
-        }
-
-        for(int i = 2; i < 18; i+=4) {
-            ImageView aisle = new ImageView(new Image("file:resources/pngs/Aisle4.png"));
-            aisle.setFitWidth(30);
-            aisle.setFitHeight(120);
-            super.board.addObject(aisle, "Aisle4" + i, true, i, 4, 0, 1);
-        }
-
-        ImageView aisle5 = new ImageView(new Image("file:resources/pngs/Aisle5.png"));
-        aisle5.setFitWidth(30);
-        aisle5.setFitHeight(150);
-        super.board.addObject(aisle5, "Aisle5.1", true, 1, 5, 18, 1);
-
-        ImageView aisle52 = new ImageView(new Image("file:resources/pngs/Aisle5.png"));
-        aisle52.setFitWidth(30);
-        aisle52.setFitHeight(150);
-        super.board.addObject(aisle52, "Aisle5.2", true, 13, 5, 18, 1);
     }
 }
